2a46c766567a4bb53cb4871d238764499537fd6a
DESKTOP-AIO2SRF\Salvatore <salvatore.arinisi@gmail.com>
2024-11-17 13:24:52 +0100

    al click di login appare la pallina, quando termina la login il componente pallina viene smontato

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-// src/App.js
 import React, { useState } from 'react';
 import './App.css';
 import AuthPage from './component/AuthPage';
 import EstrattoreAmbo from './component/EstrattoreAmbo';
 import RotatingBall from "./component/RotatingBall";
 
-
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [welcomeMessage, setLoginMessage] = useState(''); // Stato per il messaggio di benvenuto
     const [opacityToBI, setOpacityToBI] = useState(false); // Stato per l'opacità dell'immagine di background
+    const [isLoading, setIsLoading] = useState(false); // Stato per gestire RotatingBall
 
     const handleLoginSuccess = (message) => {
-        setIsAuthenticated(true);         // Imposta lo stato di autenticazione a true
-        setLoginMessage(message);       // mostra il messaggio
+        setIsAuthenticated(true); // Imposta lo stato di autenticazione a true
+        setLoginMessage(message); // Mostra il messaggio
+    };
+
+    const handleLoginStart = (stato) => {
+        setIsLoading(stato); // Attiva/disattiva RotatingBall
     };
 
     return (
         <div className={`App ${opacityToBI ? 'dimmed-background' : ''}`}>
             {!isAuthenticated ? (
-                <AuthPage setIsAuthenticated={handleLoginSuccess} />
+                <AuthPage setIsAuthenticated={handleLoginSuccess} onLoginStart={handleLoginStart} />
             ) : (
                 <>
                     {welcomeMessage && <p className="WelcomeMessage">{welcomeMessage}</p>}
-                    <EstrattoreAmbo setOpacityToBI={setOpacityToBI}/>
+                    <EstrattoreAmbo setOpacityToBI={setOpacityToBI} />
                 </>
             )}
-            <RotatingBall />
+            {isLoading && <RotatingBall />}
         </div>
     );
 }
--- src/component/AuthPage.js
+++ src/component/AuthPage.js
@@ -2,22 +2,23 @@ import React, { useState } from 'react';
 import './AuthPage.css';
 import userController from '../controller/UserController'; 
 
-function AuthPage({ setIsAuthenticated }) {
+function AuthPage({ setIsAuthenticated, onLoginStart }) {
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleLogin = async () => {
+        setErrorMessage(''); // Resetta eventuali errori precedenti
+        onLoginStart(true); // Mostra RotatingBall
 
         const nickname = "Salvatore";
         const password = "123";
         const loginResponse = await userController.login(nickname, password);
        
         if (loginResponse.status) {
-            setIsAuthenticated(loginResponse.message);  // Passa il messaggio di benvenuto
-            setErrorMessage('');  // Resetta eventuali errori precedenti
+            setIsAuthenticated(loginResponse.message); // Passa il messaggio di benvenuto
         } else {
             setErrorMessage(loginResponse.message);
         }
-
+        onLoginStart(false); 
     };
 
     const handleRegistration = () => {
