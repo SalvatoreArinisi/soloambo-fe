fe7f243304e5c6396bd386c7063b47a9930d002b
DESKTOP-AIO2SRF\Salvatore <salvatore.arinisi@gmail.com>
2024-12-01 11:05:00 +0100

    aggiunto tasto pulisci nella pagina dell'estrattore migliorato aspetto grafico generale

--- src/component/EstrattoreAmbo.js
+++ src/component/EstrattoreAmbo.js
@@ -4,25 +4,34 @@ import './Estrattore.css';
 import Ambi from "./Ambi";
 import Utility from '../Utility';
 
-function EstrattoreAmbo({ setLoading,setAmbiCaricati}) {
+function EstrattoreAmbo({ setLoading, setAmbiCaricati }) {
     const [giocata, setGiocata] = useState(null);
     const [message, setMessage] = useState('');
 
     const handleFetchAmbi = () => {
-        estraiAmbi(setGiocata, setMessage, setLoading, setAmbiCaricati); 
+        estraiAmbi(setGiocata, setMessage, setLoading, setAmbiCaricati);
+    };
+
+    const reset = () => {
+        setGiocata(null);
     };
 
     return (
         <div className="EstrattoreContainer">
             {giocata && (
                 <div className="giocata">
-                    <h2 className="message-down">giocali sulla ruota di {giocata.ruota} il {Utility.estraiGiornoEMese(giocata.dataEstrazione)}</h2>                 
+                    <p className="message-down">giocali sulla ruota di {giocata.ruota} il {Utility.estraiGiornoEMese(giocata.dataEstrazione)}</p>
                     <Ambi ambi={giocata.ambi} />
                     <p className="message-up">{message}</p>
                 </div>
             )}
             <div className="ButtonContainer">
-                <button onClick={handleFetchAmbi}>Estrai Ambi</button>
+                <button onClick={handleFetchAmbi} style={{
+                    display: giocata ? "none" : "block",
+                }}>Estrai Ambi</button>
+                <button onClick={reset} style={{
+                    display: giocata ? "block" : "none",
+                }}>Pulisci</button>
             </div>
         </div>
     );
